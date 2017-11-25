32409934e61a79b4205f0979361cae1ea7e25b70
Giovanni Filaferro <g.filaferro@nanotek.it>
2017-11-25 17:17:34 +0100

    iOS App Implementation start, dependencies

---
+++ Implementation/App/Travlendar/Pods/Target Support Files/Pods-Travlendar/Pods-Travlendar-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Travlendar : NSObject
+@end
+@implementation PodsDummy_Pods_Travlendar
+@end
---
+++ Implementation/App/Travlendar/Pods/Target Support Files/Pods-TravlendarTests/Pods-TravlendarTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TravlendarTests : NSObject
+@end
+@implementation PodsDummy_Pods_TravlendarTests
+@end
