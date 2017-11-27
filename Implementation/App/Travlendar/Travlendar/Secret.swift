bb1594c1d994005ef4fa5f403b7f039ec2bb94a9
Giovanni Filaferro <g.filaferro@nanotek.it>
2017-11-27 09:50:32 +0100

    travis jobs

---
+++ Implementation/App/Travlendar/Travlendar/Secret.swift
@@ -0,0 +1,18 @@
+//
+//  Secret.swift
+//  Travlendar
+//
+//  Created by Giovanni Filaferro on 27/11/2017.
+//  Copyright © 2017 Giovanni Filaferro. All rights reserved.
+//
+
+import UIKit
+import KeychainSwift
+
+class Secret: NSObject {
+    
+    static let shared: Secret = Secret()
+    
+    
+    
+}
