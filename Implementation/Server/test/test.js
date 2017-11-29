a8cf6557928e66eec9402bbadb24e5abd3082643
Benedetto Nespoli <benedetto.nespoli@gmail.com>
2017-11-29 13:36:13 +0100

    added some test, still no terminating

--- Implementation/Server/test/test.js
+++ Implementation/Server/test/test.js
@@ -2,7 +2,7 @@ const app = require("../index")
 const request = require('supertest');
 const assert = require('assert');
 
-describe('GET /hello', function () {
+describe('GET /hello', function (t) {
 	it('should return hello', function () {
 		request(app)
 			.get("/hello")
@@ -11,3 +11,27 @@ describe('GET /hello', function () {
 			})
 	});
 });
+
+describe('Authorization', function () {
+	it('should expect a 401 if not authorization is provided', function (done) {
+		request(app)
+			.get("/helloWithAuth")
+			.expect(401)
+			.end((err, res) => {
+				if (err) done(err)
+				else done()
+			})
+	});
+
+	it('should expect a 403 if invalid authorization is provided', function (done) {
+		request(app)
+			.get("/helloWithAuth")
+			.set("X-Access-Token", "fail")
+			.expect(403)
+			.end((err, res) => {
+				if (err) done(err)
+				else done()
+			})
+	});
+});
+
