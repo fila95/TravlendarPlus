4aca0bd4624c92bf020a2827aa81004cccefe059
Benedetto Nespoli <benedetto.nespoli@gmail.com>
2017-12-03 00:10:39 +0100

    added calendars post and its tests

--- Implementation/Server/routes/calendars.js
+++ Implementation/Server/routes/calendars.js
@@ -1,9 +1,39 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/calendars', function (req, res) {
+// GET
+router.get('/calendars', (req, res) => {
 	req.models.calendars.find({ user_id: req.user.id }, (err, results) => {
 		res.json(results).end()
+	})
+})
+
+// Example of valid color: #F203b1
+let validColor = (color) => {
+	return color.match("#[0-9a-fA-F]{6}")
+}
+
+// POST
+router.post('/calendars', (req, res) => {
+	// Validate inputs
+	if (!req.body.name || !req.body.color) {
+		return res.sendStatus(400).end()
+	}
+	let name = req.body.name.trim()
+	let color = req.body.color.trim()
+	if(!validColor(color)) {
+		return res.sendStatus(400).end()
+	}
+
+	// Create the calendar
+	req.models.calendars.create({
+		user_id: req.user.id,
+		name: name,
+		color: color
+	}, (err, result) => {
+		if (err) return res.sendStatus(500).end()
+		
+		return res.json(result).end()
 	})
 })
 
--- Implementation/Server/test/test.js
+++ Implementation/Server/test/test.js
@@ -1,4 +1,4 @@
-const app = require("../index")
+const app = require('../index')
 const request = require('supertest')
 const assert = require('assert')
 const uuidv4 = require('uuid/v4')
@@ -6,30 +6,16 @@ const uuidv4 = require('uuid/v4')
 const user_token = uuidv4()
 let access_token = null
 
-describe('POST /login', (t) => {
-	it('should create a user and return a new access_token', (done) => {
-		request(app)
-			.post("/api/v1/login")
-			.send({ 'user_token': user_token })
-			.type('form')
-			.expect(200)
-			.expect(res => {
-				if (res.body.access_token == undefined) {
-					throw new Error(JSON.stringify(res.body))
-				}
-			})
-			.end(done)
-	})
-
+describe('POST /login', () => {
 	it('should return a new access_token if a valid user_token is provided', (done) => {
 		request(app)
-			.post("/api/v1/login")
+			.post('/api/v1/login')
 			.send({ 'user_token': user_token })
 			.type('form')
 			.expect(200)
 			.expect(res => {
 				if (res.body.access_token == undefined) {
-					throw new Error("No access_token in response")
+					throw new Error('No access_token in response')
 				}
 				access_token = res.body.access_token
 			})
@@ -38,30 +24,30 @@ describe('POST /login', (t) => {
 
 	it('should return an error 401 if no user token is provided', (done) => {
 		request(app)
-			.post("/api/v1/login")
+			.post('/api/v1/login')
 			.expect(401)
 			.end(done)
 	})
 
 	it('should return an error 403 if invalid user token is provided', (done) => {
 		request(app)
-			.post("/api/v1/login")
-			.send({ 'user_token': "fail" })
+			.post('/api/v1/login')
+			.send({ 'user_token': 'fail' })
 			.type('form')
 			.expect(403)
 			.end(done)
 	})
 })
 
-describe('GET /calendars', (t) => {
+describe('GET /calendars', () => {
 	it('should return an empty list of calendars if a valid access token is provided', (done) => {
 		request(app)
-			.get("/api/v1/calendars")
+			.get('/api/v1/calendars')
 			.set('X-Access-Token', access_token)
 			.expect(200)
 			.expect(res => {
 				if (res.body === {}) {
-					throw new Error("No empty list")
+					throw new Error('No empty list')
 				}
 			})
 			.end(done)
@@ -69,16 +55,36 @@ describe('GET /calendars', (t) => {
 
 	it('should return an error 401 if no access token is provided', (done) => {
 		request(app)
-			.get("/api/v1/calendars")
+			.get('/api/v1/calendars')
 			.expect(401)
 			.end(done)
 	})
 
 	it('should return an error 403 if invalid access token is provided', (done) => {
 		request(app)
-			.get("/api/v1/calendars")
-			.set('X-Access-Token', "fail")
+			.get('/api/v1/calendars')
+			.set('X-Access-Token', 'fail')
 			.expect(403)
+			.end(done)
+	})
+})
+
+describe('POST /calendars', () => {
+	it('should create a calendar if a valid access token is provided', (done) => {
+		request(app)
+			.post('/api/v1/calendars')
+			.set('X-Access-Token', access_token)
+			.send({
+				'name': 'name test',
+				'color': '#1278EF'
+			})
+			.type('form')
+			.expect(200)
+			.expect(res => {
+				if (!res.body.id) {
+					throw new Error('No calendar returned')
+				}
+			})
 			.end(done)
 	})
 })
