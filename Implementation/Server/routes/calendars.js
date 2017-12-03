144dc846a3bb0035bf7880c4f0ffa41a5631b5c7
Benedetto Nespoli <benedetto.nespoli@gmail.com>
2017-12-03 14:16:53 +0100

    comments on calendars

--- Implementation/Server/routes/calendars.js
+++ Implementation/Server/routes/calendars.js
@@ -1,19 +1,20 @@
 const express = require('express')
 const router = express.Router()
 
-// GET
+// Return a list of User's Calendars
 router.get('/calendars', (req, res) => {
 	req.models.calendars.find({ user_id: req.user.id }, (err, results) => {
 		res.json(results).end()
 	})
 })
 
+// Function that returns true if the color is valid
 // Example of valid color: #F203b1
 let validColor = (color) => {
 	return color.match("#[0-9a-fA-F]{6}")
 }
 
-// POST
+// Create a new Calendar with the name and color specified
 router.post('/calendars', (req, res) => {
 	// Validate inputs
 	if (!req.body.name || !req.body.color) {
