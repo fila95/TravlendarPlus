1505958a733072d11f5618b198ccf78ef0015d10
Benedetto Nespoli <benedetto.nespoli@gmail.com>
2017-12-03 18:30:23 +0100

    rewritten part of security tests

--- Implementation/Server/test/security.js
+++ Implementation/Server/test/security.js
@@ -3,24 +3,32 @@ const request = require('supertest')
 
 const baseURL = "/api/v1/"
 const endpoints = [
-	"/calendars"
+	{
+		"endpoint": "/calendars",
+		"verbs": ["GET", "PUT"]
+	}
 ]
 
-endpoints.forEach((endpoint) => {
-	describe('Security test on ' + endpoint, () => {
-		it('should return an error 401 if no access token is provided', (done) => {
-			request(app)
-				.get('/api/v1/' + endpoint)
-				.expect(401)
-				.end(done)
-		})
+describe('Security tests', () => {
+	endpoints.forEach((obj) => {
+		let endpoint = obj.endpoint
+		obj.verbs.forEach((verb) => {
+			describe(verb + ' ' + endpoint, () => {
+				it('should return an error 401 if no access token is provided', (done) => {
+					request(app)
+					[verb.toLowerCase()]('/api/v1/' + endpoint)
+						.expect(401)
+						.end(done)
+				})
 
-		it('should return an error 403 if invalid access token is provided', (done) => {
-			request(app)
-				.get('/api/v1/' + endpoint)
-				.set('X-Access-Token', 'fail')
-				.expect(403)
-				.end(done)
+				it('should return an error 403 if invalid access token is provided', (done) => {
+					request(app)
+					[verb.toLowerCase()]('/api/v1/' + endpoint)
+						.set('X-Access-Token', 'invalid access_token')
+						.expect(403)
+						.end(done)
+				})
+			})
 		})
 	})
 })
