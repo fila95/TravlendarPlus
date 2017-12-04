8eb62df38e6088901e018bab05e132453de836ce
Benedetto Nespoli <benedetto.nespoli@gmail.com>
2017-12-04 19:11:18 +0100

    basic CRUD events

---
+++ Implementation/Server/routes/events.js
@@ -0,0 +1,96 @@
+const express = require('express')
+const router = express.Router({ mergeParams: true })
+
+// Return a list of User's Events
+router.get('/', (req, res) => {
+	req.models.users.find({ id: req.user.id }, (err, users) => {
+		if (err) throw err
+		let calendars = {}
+		/*users[0].getCalendars().each(calendar => {
+			calendar.getEvents((events) => {
+				calendars[calendar.id] = events
+			})
+		}).get(() => {
+			console.log(calendars)
+			return res.json(calendars).end()
+		})*/
+
+		users[0].getCalendars((err, calendars) =>  {
+			calendar_ids = []
+			for (calendar of calendars) {
+				calendar_ids.push(calendar.id)
+			}
+			req.models.events.find({ calendar_id: calendar_ids }, (err, events) => {
+				return res.json(events).end()
+			})
+		})
+	})
+})
+
+// Create a new Event with the parameters specified
+router.put('/', (req, res) => {
+	// Validate inputs:
+	// These three fields are mandatory, meaning that if them are null or invalid,
+	// then a 400 error is thrown.
+	// All the other fields are optionals, meaning that if them are null or invalid,
+	// they are just ignored and the default value (defined in models.js) is used
+	let title = req.body.title
+	let start_time = new Date(req.body.start_time || undefined)
+	let end_time = new Date(req.body.end_time || undefined)
+	if (!title || !start_time || !end_time) {
+		return res.sendStatus(400).end()
+	}
+
+	// Creating initial object
+	let event = {
+		calendar_id: req.params.calendar_id,
+		title: req.body.title.trim(),
+		start_time: start_time,
+		end_time: end_time
+	}
+
+	// Adding all the optional fields
+	let address = (req.body.address || '').trim()
+	if (address) event.address = address
+
+	let lat = parseFloat(req.body.lat)
+	let lng = parseFloat(req.body.lng)
+	if (lat && lng) {
+		event.lat = lat
+		event.lng = lng
+	}
+
+	let duration = parseInt(req.body.duration)
+	if (duration) event.duration = duration
+
+	let repetitions = (req.body.repetitions || '').trim()
+	if (repetitions.match("0b[01]{7}")) {
+		event.repetitions = repetitions
+	}
+
+	let transports = (req.body.transports || '').trim()
+	if (transports.match("0b[01]{5}")) {
+		event.transports = transports
+	}
+
+	// Create the event
+	req.models.events.create(event, (err, result) => {
+		if (err) return res.sendStatus(500).end()
+
+		return res.status(201).json(result).end()
+	})
+
+})
+
+router.delete('/:event_id', (req, res) => {
+	// Delete the event
+	req.models.events.find({
+		id: req.params.event_id
+	}).remove((err, result) => {
+		if (err) return res.sendStatus(500).end()
+
+		return res.status(200).json(result).end()
+	})
+})
+
+module.exports = router
---
+++ Implementation/Server/test/events.js
@@ -0,0 +1,146 @@
+const app = require('../index')
+const request = require('supertest')
+const crypto = require('crypto')
+const uuidv4 = require('uuid/v4')
+const orm = require('orm')
+
+const eventTitle = 'valid event name'
+const startTime = '2017-12-04T11:00:47.676Z'
+const endTime = '2017-12-04T13:01:40.143Z'
+
+let db = null
+let user = null
+let device = null
+let calendar = null
+let event = null
+let access_token = crypto.randomBytes(48).toString('base64')
+
+let createData = (done) => {
+	const user_token = uuidv4()
+	db.models.users.create({
+		user_token: user_token
+	}, (err, _user) => {
+		if (err) throw err
+		user = _user
+		db.models.devices.create({
+			user_id: user.id,
+			access_token: access_token
+		}, (err, _device) => {
+			if (err) throw err
+			device = _device
+			db.models.calendars.create({
+				user_id: user.id,
+				name: "valid calendar name",
+				color: "#FF0000"
+			}, (err, _calendar) => {
+				if (err) throw err
+				calendar = _calendar
+				done()
+			})
+		})
+	})
+}
+
+describe('Events API', () => {
+	before((done) => {
+		// Connect to database
+		// Create a test user and device
+		if (!app.get('db')) {
+			app.on('db_connected', (_db) => {
+				db = _db
+				createData(done)
+			})
+		} else {
+			db = app.get('db')
+			createData(done)
+		}
+	})
+
+	after((done) => {
+		// Delete the test user, device, calendar and event created during the test
+		db.models.users.find({ id: user.id }).remove(() => {
+			db.models.devices.find({ id: device.id }).remove(() => {
+				db.models.calendars.find({ id: calendar.id }).remove(() => {
+					db.models.events.find({ id: event.id }).remove(() => {
+						done()
+					})
+				})
+			})
+		})
+	})
+
+	describe('PUT /events', () => {
+		let _event = {
+			'title': eventTitle,
+			'start_time': startTime,
+			'end_time': endTime
+		}
+
+		it('should create an event if a valid access token is provided', (done) => {
+			request(app)
+				.put('/api/v1/calendars/' + calendar.id + '/events')
+				.set('X-Access-Token', device.access_token)
+				.send(_event)
+				.type('form')
+				.expect(201)
+				.expect(res => {
+					if (!res.body.id) {
+						throw new Error('No calendar returned')
+					}
+					event = res.body
+				})
+				.end(done)
+		})
+
+		/* TODO: fix routes/events.js in order to not allow overlapping events
+		 * For now it returns a 201 Created
+		it('should throw a 500 error creating an event overlapping', (done) => {
+			request(app)
+				.put('/api/v1/calendars/'+calendar.id+'/events')
+				.set('X-Access-Token', device.access_token)
+				.send(event)
+				.type('form')
+				.expect(500)
+				.end(done)
+		})*/
+
+		it('should throw a 400 error creating an event with an invalid parameter', (done) => {
+			request(app)
+				.put('/api/v1/calendars/' + calendar.id + '/events')
+				.set('X-Access-Token', device.access_token)
+				.send({
+					'name': '',
+					'start_time': startTime,
+					'end_time': endTime
+				})
+				.type('form')
+				.expect(400)
+				.end(done)
+		})
+	})
+
+	describe('GET /events', () => {
+		it('should return a list of events if a valid access token is provided', (done) => {
+			request(app)
+				.get('/api/v1/calendars/' + calendar.id + '/events')
+				.set('X-Access-Token', device.access_token)
+				.expect(200)
+				.expect(res => {
+					if (!res.body || res.body.length != 1) {
+						throw new Error('No event list received')
+					}
+				})
+				.end(done)
+		})
+	})
+
+	describe('DELETE /events/:id', () => {
+		it('should delete the event if a valid access token is provided', (done) => {
+			request(app)
+				.delete('/api/v1/calendars/' + calendar.id + '/events/' + event.id)
+				.set('X-Access-Token', device.access_token)
+				.expect(200)
+				.end(done)
+		})
+	})
+})
