954e9baa5b7e64d65c60366042512a7ff9459186
Benedetto Nespoli <benedetto.nespoli@gmail.com>
2017-12-07 15:34:42 +0100

    removed useless comments

--- Implementation/Server/routes/index.js
+++ Implementation/Server/routes/index.js
@@ -2,15 +2,6 @@ const express = require('express')
 const auth = require('../auth')
 const router = express.Router()
 
-/*app.get("/hello", (req, res) => {
-	res.end("hello")
-})
-
-app.get("/helloWithAuth", (req, res) => {
-	res.json(req.user)
-	res.end()
-})*/
-
 router.use('/api/v1/', require('./login'))
 router.use('/api/v1/calendars', auth, require('./calendars'))
 router.use('/api/v1/calendars/:calendar_id/events', auth, require('./events'))
