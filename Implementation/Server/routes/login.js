b33cc9a18cdb51ca201b39e908ffa286e7a521d4
Benedetto Nespoli <benedetto.nespoli@gmail.com>
2017-12-07 14:33:41 +0100

    now creating settings with users

--- Implementation/Server/routes/login.js
+++ Implementation/Server/routes/login.js
@@ -7,18 +7,22 @@ let generateToken = () => {
 	return crypto.randomBytes(48).toString('base64')
 }
 
+// Create settings in the database for the user user
+let createSettings = (user, req, cb) => {
+	req.models.settings.create({}, (err, settings) => {
+		user.setSettings(settings, cb)
+	})
+}
+
 // Create a device in the database for the user user and
 // return an object containing the access token
-let createDevice = (user, req, res) => {
+let createDevice = (user, req, cb) => {
 	let access_token = generateToken()
 	req.models.devices.create({
 		user_id: user.id,
 		access_token: access_token,
 		device_type: req.body.device_type || null
-	}, err => {
-		//if (err) throw err
-		res.json({ access_token: access_token }).end()
-	})
+	}, cb)
 }
 
 router.post('/login', function (req, res) {
@@ -27,6 +31,7 @@ router.post('/login', function (req, res) {
 	}
 
 	req.models.users.find({ user_token: req.body.user_token }, (err, users) => {
+		// No users exists with that user_token => create it
 		if (users.length == 0) {
 			// Check if it's a valid UUID
 			if (!validate(req.body.user_token, 4)) {
@@ -35,13 +40,18 @@ router.post('/login', function (req, res) {
 			// Create the user and the device
 			req.models.users.create({
 				user_token: req.body.user_token
-			}, (err, result) => {
-				//if (err) throw err
-				createDevice(result, req, res)
+			}, (err, user) => {
+				createDevice(user, req, (err, device) => {
+					createSettings(user, req, (err, settings) => {
+						res.json({ access_token: device.access_token }).end()
+					})
+				})
 			})
 		} else {
-			// Create only the device
-			createDevice(users[0], req, res)
+			// The user already exists, just create the device
+			createDevice(users[0], req, (err, device) => {
+				res.json({ access_token: device.access_token }).end()
+			})
 		}
 	})
 
