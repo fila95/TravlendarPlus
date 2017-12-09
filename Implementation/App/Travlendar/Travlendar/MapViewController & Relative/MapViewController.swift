608d7124b59853486183816474d2784b5521ced1
Giovanni Filaferro <g.filaferro@nanotek.it>
2017-12-09 19:56:14 +0100

    App

---
+++ Implementation/App/Travlendar/Travlendar/MapViewController & Relative/MapViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MapViewController.swift
+//  Travlendar
+//
+//  Created by Giovanni Filaferro on 09/12/2017.
+//  Copyright © 2017 Giovanni Filaferro. All rights reserved.
+//
+
+import UIKit
+
+
+class MapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+
