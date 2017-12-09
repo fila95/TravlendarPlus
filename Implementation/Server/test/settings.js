87deb5a68c7b6997c62480bb48051f8f81a969af
Benedetto Nespoli <benedetto.nespoli@gmail.com>
2017-12-09 18:55:48 +0100

    Added a general environment for all the tests with a created user, device and settings

--- Implementation/Server/test/settings.js
+++ Implementation/Server/test/settings.js
@@ -1,54 +1,12 @@
 const app = require('../index')
-const crypto = require('crypto')
-const uuidv4 = require('uuid/v4')
 const request = require('supertest')
 
-let user = {}
-let db = null
 let device = null
-let access_token = crypto.randomBytes(48).toString('base64')
-
-let createData = (done) => {
-	const user_token = uuidv4()
-	request(app)
-		.post('/api/v1/login')
-		.send({ 'user_token': user_token })
-		.type('form')
-		.expect(200)
-		.expect(res => {
-			if (res.body.access_token == undefined) {
-				throw new Error('No access_token in response')
-			}
-			device = res.body
-			user.id = device.user_id
-		})
-		.end(done)
-}
 
 describe('Settings API', () => {
 	before((done) => {
-		// Connect to database
-		// Create a test user and device
-		if (!app.get('db')) {
-			app.on('db_connected', (_db) => {
-				db = _db
-				createData(done)
-			})
-		} else {
-			db = app.get('db')
-			createData(done)
-		}
-	})
-
-	after((done) => {
-		// Delete the test user, device and settings created during the test
-		db.models.users.find({ id: user.id }).remove(() => {
-			db.models.devices.find({ id: device.id }).remove(() => {
-				db.models.settings.find({ user_id: user.id }).remove(() => {
-					done()
-				})
-			})
-		})
+		device = app.get('testData').device
+		done()
 	})
 
 	describe('GET /settings', () => {
---
+++ Implementation/Server/test/setup.js
@@ -0,0 +1,66 @@
+const app = require('../index')
+const request = require('supertest')
+const crypto = require('crypto')
+const uuidv4 = require('uuid/v4')
+
+let user, device
+
+let createUser = (cb) => {
+	db.models.users.create({
+		user_token: uuidv4()
+	}, (err, _user) => {
+		if (err) throw err
+		user = _user
+		db.models.settings.create({
+			user_id: user.id
+		}, (err, _settings) => {
+			if (err) throw err
+			cb()
+		})
+	})
+}
+
+let createDevice = (cb) => {
+	db.models.devices.create({
+		user_id: user.id,
+		access_token: crypto.randomBytes(48).toString('base64')
+	}, (err, _device) => {
+		if (err) throw err
+		device = _device
+		cb()
+	})
+}
+
+// Create a test user and device
+let createData = (cb) => {
+	createUser(() => {
+		createDevice(() => {
+			app.set('testData', { user: user, device: device })
+			cb()
+		})
+	})
+
+}
+
+before((done) => {
+	// Connect to database
+	if (!app.get('db')) {
+		app.on('db_connected', (_db) => {
+			db = _db
+			createData(done)
+		})
+	} else {
+		db = app.get('db')
+		createData(done)
+	}
+})
+
+after((done) => {
+	device.remove(() => {
+		db.models.settings.find({ user_id: user.id }).first((err, settings) => {
+			settings.remove(() => {
+				user.remove(done)
+			})
+		})
+	})
+})
