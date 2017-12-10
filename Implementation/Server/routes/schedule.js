4dfc0e20a73e43717c246acdfb7ee7a0e065f986
Benedetto Nespoli <benedetto.nespoli@gmail.com>
2017-12-10 16:43:58 +0100

    added some schedule functions

--- Implementation/Server/routes/schedule.js
+++ Implementation/Server/routes/schedule.js
@@ -1,36 +1,53 @@
 const express = require('express')
 const router = express.Router()
 
-/*
-for (var i = 0, len = calendars.length; i < len; i++) {
-	id = calendars[i]['id']
-	console.log("ID: " + id)
-	req.models.events.find({ calendar_id: id }, (err2, events) => {
-		console.log("ID: " + id)
-		if (!events) {
-			calendars[i]['calendar_events'] = null
-		} else {
-			calendars[i]['calendar_events'] = null
-		}
+// Suppose that timeSlots are sorted and not overlapping
+// Return the time in milliseconds for the given timeSlots
+let freeTimeForTimeSlots = timeSlots => {
+	let freeTime = 0
+	for (timeSlot of timeSlots) {
+		freeTime += (timeSlots.end_time - timeSlots.start_time)
+	}
+	return freeTime
+}
+
+// Suppose that timeSlots are sorted and not overlapping
+// Return the total time in milliseconds in which the flexibleEvent cannot be scheduled
+let occupiedTimeForTimeSlots = (flexibleEvent, timeSlots) => {
+	let free = freeTimeForTimeSlots(timeSlots)
+	let window = flexibleEvent.end_time - flexibleEvent.start_time
+	return window - free
+}
+
+// Return a number from 0 to 1 where:
+//   numbers near to 0 means 'quite fixed event'
+//   numbers near to 1 means 'very flexible event'
+let fitness = (event, timeSlots) => {
+	return 1 - (event.duration / (event.end_time - event.start_time - occupiedTimeForTimeSlots(event, timeSlots)))
+}
+
+// Sort the flexible events looking at the fitness function:
+// The events that are less flexible are the first, and the events that are the most flexible are the last
+let sortWithFitness = (eventList, timeSlots) => {
+	return eventList.sort((a, b) => {
+		return fitness(a, timeSlots) - fitness(b, timeSlots)
 	})
 }
-*/
 
 // Sort the event looking at start_time in ascendent order
 let sort = eventList => {
 	return eventList.sort((a, b) => {
-		// FIXME: mettere a posto in modo che funzioni con le date
 		return a.start_time - b.start_time
 	})
 }
 
-// Return true if there are at least two events that are overlapping, otherwise false
+// Return two event that are overlapping, if there are, otherwise false
 // Computational time: O(nlogn)
 let overlap = eventList => {
 	eventList = sort(eventList)
 	for (let i = 0; i < eventList.length - 1; i++) {
 		if (eventList[i].end_time > eventList[i + 1].start_time) {
-			return true
+			return [eventList[i].id, eventList[i + 1].id]
 		}
 	}
 	return false
@@ -68,16 +85,27 @@ let timeSlots = (fixedEventList, flexibleEvent) => {
 	return timeSlots
 }
 
-// Return a list of User's Calenars
 router.get('/', (req, res) => {
 	req.user.getCalendars().each((err, calendar) => {
+		// TODO prendere tutti gli eventi di OGGI
 		calendar.getEvents((err, events) => {
+			let overlapping = overlap(events)
+			if (overlapping) return res.status(400).end('overlapping: ' + overlapping)
 			// TODO
-			if (overlap(events)) return res.status(400).end('overlapping')
-
-
 		})
 	})
 })
 
 module.exports = router
+
+if(process.env.ENV=='testing') {
+	router.testFunctions = {
+		timeSlots: timeSlots,
+		overlap: overlap,
+		sort: sort,
+		sortWithFitness: sortWithFitness,
+		fitness: fitness,
+		occupiedTimeForTimeSlots: occupiedTimeForTimeSlots,
+		freeTimeForTimeSlots: freeTimeForTimeSlots
+	}
+}
