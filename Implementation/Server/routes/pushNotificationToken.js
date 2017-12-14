f13d0076f04376abafd3d49aabf821da9a9f849e
Benedetto Nespoli <benedetto.nespoli@gmail.com>
2017-12-14 20:48:41 +0100

    removed useless code

--- Implementation/Server/routes/pushNotificationToken.js
+++ Implementation/Server/routes/pushNotificationToken.js
@@ -5,14 +5,7 @@ router.put('/', (req, res) => {
 	if (!req.body.token) {
 		return res.sendStatus(400).end()
 	}
-	if (!req.get('X-Access-Token')) {
-		return res.sendStatus(401).end()
-	}
 	req.models.devices.find({ access_token: req.get('X-Access-Token') }).first((err, device) => {
-		if (err || !device) {
-			return res.sendStatus(403).end()
-		}
-
 		device.push_token = req.body.token
 		device.save(() => {
 			return res.status(204).end()
