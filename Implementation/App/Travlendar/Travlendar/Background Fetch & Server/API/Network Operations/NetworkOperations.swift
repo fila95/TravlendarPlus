c7db29cb05bd9ef0595a8a5d85c17a0390b1e790
Giovanni Filaferro <fila95@live.it>
2018-01-07 12:26:55 +0100

    added unavailable error handling

--- Implementation/App/Travlendar/Travlendar/Background Fetch & Server/API/Network Operations/NetworkOperations.swift
+++ Implementation/App/Travlendar/Travlendar/Background Fetch & Server/API/Network Operations/NetworkOperations.swift
@@ -18,17 +18,19 @@ enum NStatusCode: Int {
     case forbidden = 403
     case notfound = 404
     case internalServerError = 500
+    case unavailable = 503
     
     public var description: String {
         switch self {
-        case .ok: return "ok"
-        case .created: return "created"
-        case .okNoContentNeeded: return "okNoContentNeeded"
-        case .badRequest: return "badRequest"
-        case .unauthorized: return "unauthorized"
-        case .forbidden: return "forbidden"
-        case .notfound: return "not found"
-        case .internalServerError: return "internalServerError"
+            case .ok: return "ok"
+            case .created: return "created"
+            case .okNoContentNeeded: return "okNoContentNeeded"
+            case .badRequest: return "badRequest"
+            case .unauthorized: return "unauthorized"
+            case .forbidden: return "forbidden"
+            case .notfound: return "not found"
+            case .internalServerError: return "internalServerError"
+            case .unavailable: return "unavailable"
         }
     }
 }
@@ -104,9 +106,9 @@ class NetworkOperation: Operation {
                 self.completionHandler?(false, "Response Unavailable")
                 print("Error \(endpoint.capitalized) Operation: \n\tResponse unavailable")
                 
-                print(String.init(data: data!, encoding: .utf8))
-                print(response)
-                print(error)
+//                print(String.init(data: data!, encoding: .utf8))
+//                print(response)
+//                print(error)
                 return
             }
             
